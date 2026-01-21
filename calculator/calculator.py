8302eba7336b8b2fd48c8260953d3146a47c1fb6
Sophia <joyul3433@gmail.com>
2026-01-21 14:17:36 +0000

    added add function

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,3 +1,9 @@
+def add():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter first number: "))
+
+    print(f'{a} + {b} = {a + b}')
+
 while True:
     try:
         print("Options: ")
