07b79c69625b471a8bf5d5d7b1dcb5b31b2c34bf
Aarongel205 <aaronadanza92@gmail.com>
2026-02-09 21:38:34 +0800

    added multithreading and multiprocessing

---
+++ gradeComputingSystem/multiProcessing.py
@@ -0,0 +1,38 @@
+#Multiprocessing Snippet
+
+from multiprocessing import Process
+import time
+
+def compute_gwa_mp(grades):
+
+    gwa = sum(grades) / len(grades)
+    
+    print(f"[Process] Calculated GWA: {gwa}")
+
+if __name__ == "__main__":
+    grades_list = [] # Replace with user input
+
+    for i in range(5):
+        grades = int(input("Enter grades: "))
+        grades_list.append(grades)
+
+    time_start = time.perf_counter()
+        
+    processes = []
+
+    for grade in grades_list:
+
+        p = Process(target=compute_gwa_mp, args=([grade],))
+        
+        processes.append(p)
+        
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    time_end = time.perf_counter()
+
+    execution_time = time_end - time_start
+
+    print(f'Excecution time {execution_time}')
---
+++ gradeComputingSystem/multiThreading.py
@@ -0,0 +1,37 @@
+#Multithreading Snippet
+import threading
+import time
+
+def compute_gwa(grades):
+    gwa = sum(grades) / len(grades)
+    
+    print(f"[Thread] Calculated GWA: {gwa}")
+
+if __name__ == "__main__":
+    grades_list = []
+
+    for i in range(5):
+        grades = int(input("Enter grades: "))
+        
+        grades_list.append(grades)
+
+    time_start = time.perf_counter()
+
+    threads = []
+
+    for grade in grades_list:
+        t = threading.Thread(target=compute_gwa, args=([grade],))
+        
+        threads.append(t)
+        
+        t.start()
+
+    for t in threads:
+
+        t.join()
+
+    time_end = time.perf_counter()
+
+    execution_time = time_end - time_start
+
+    print(f'Execution time: {execution_time}')
