4db5845acce39d9baaa9bbbc9e6523f0a3c52167
nirosval <nirosvalinojales03@gmail.com>
2026-02-20 22:59:59 +0800

    added important library, initialize TPE & process_patient function

--- parallelChallenge/sequential.py
+++ parallelChallenge/sequential.py
@@ -13,3 +13,15 @@ for patient in patients:
 end = time.time()
 
 print("Sequential Time:", round(end - start, 2), "seconds")
+
+
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+def process_patient(patient_id):
+    time.sleep(1)
+    time.sleep(1)
+    time.sleep(2)
+
+
+
